9a4be26d0e852ba7e8351c157b055eb7a9accf08
Minh-Thai <minhtyyufa@gmail.com>
2021-05-03 14:24:20 -0400

    maze solving

--- motor_test.ino
+++ motor_test.ino
@@ -4,6 +4,8 @@
 MotorController motorController;
 
 SensorArray* sensorArray = NULL;
+unsigned long time_since_not_left = millis();
+
 void setup() {
   Serial.begin(9600);
 
@@ -44,24 +46,51 @@ void loop(){
   
 }
 
+void goLeftUntilResetPoint(){
+  //bool resetPointHit = false;
+  bool leftMostTurnedOff = false;
+  bool middles[3];
+  while(1){
+    if(!sensorArray->isLeftMostOn()){
+      leftMostTurnedOff = true;  
+    }
+    if(leftMostTurnedOff){
+      sensorArray->getMiddles(middles);
+      if(middles[0] && middles[1] && middles[2]){
+        break;
+      }
+    }   
+  }
+}
 
 void decideWhereToGo(){
   bool middles[3];
   sensorArray->getMiddles(middles);
-  if(sensorArray->isLeftMostOn()){
- 
-    motorController.turn_left(128);
-  } else if(middles[0] && middles[2]){
-    Serial.println("I go straight");
-    motorController.go_straight(128);
-  }  else if (middles[0] && !middles[2]){
+  bool ignoreLeft = millis() - time_since_not_left > 1000;
+  
+  if (!ignoreLeft && middles[0] && !middles[2]){
     Serial.println("turn left");
     motorController.turn_left(128);
+  } else if(!ignoreLeft && sensorArray->isLeftMostOn()){
+    
+    motorController.turn_left(128);
+    delay(1000);
+  } else if(!ignoreLeft && middles[0] && middles[2]){
+    Serial.println("I go straight");
+    motorController.go_straight(128);
   } else if (!middles[0] && middles[2]){
+    
 //        Serial.println("turn right");
+    time_since_not_left = millis();
     motorController.turn_right(128);  
+    if(ignoreLeft)
+      delay(500);
   }else if (sensorArray->isRightMostOn()) {
+    time_since_not_left = millis();
     motorController.turn_right(128);
+    if(ignoreLeft)
+      delay(500);
+    
   } else {
     //motorController.brake();
   }
